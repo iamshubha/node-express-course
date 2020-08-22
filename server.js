408eb04d8c51a8b6fe2e09c4971151006bd9cca2
shubhabanerjee <shubhabanerjeewin8@gmail.com>
2020-08-22 12:09:02 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 
-const mockUserData=[
-	{name:'Mark'},
-	{name:'Jill'}
+const mockUserData = [
+    { name: 'Mark' },
+    { name: 'Jill' }
 ]
 
-app.get('/users',function(req,res){
-	res.json({
-		success: true,
-		message: 'successfully got users. Nice!',
-		users: mockUserData
-	})
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
 })
-
-app.listen(8000,function(){console.log('server is listening')})
+app.get('/g', function (req, res) {
+    res.send({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: gg
+    })
+})
+const gg = [
+    { a: "ghghg" },
+    { b: "hvgcfx" }
+]
+app.listen(8000, function () { console.log('server is listening') })
